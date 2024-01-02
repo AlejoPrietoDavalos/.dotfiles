2d3255330fbe596b0755dfd2484bd6640545c98e
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-01-01 21:32:55 -0300

    asd

--- fonts/README.md
+++
@@ -1,4 +0,0 @@
-Las fonts se tienen que poner en:
-/usr/local/share/fonts
-
-En las settings de la terminal se puede settear la fuente.
--- fonts/install_fonts.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def main() -> None:
-    os.system("sudo cp ./*.ttf /usr/local/share/fonts")
-
-if __name__ == "__main__":
-    main()
